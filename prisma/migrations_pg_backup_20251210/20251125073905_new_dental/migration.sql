ce66dc4e66606d464ebec8a3cd6717e1a61a68ff
hamed <hamed.sepehrnia85@gmail.com>
2025-12-26 23:08:08 +0330

    Refactor migration scripts to improve error handling and user instructions, ensuring better clarity and accessibility. Update seedDoctors.js for consistent English translations in console outputs.

--- prisma/migrations_pg_backup_20251210/20251125073905_new_dental/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "comments" ADD COLUMN     "parent_id" TEXT;
-
--- AlterTable
-ALTER TABLE "doctors" ADD COLUMN     "short_description" TEXT;
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
